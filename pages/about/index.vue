3842e27e95d3f900a1c68c39cd612696985c70f4
Singh, Atin <>
2020-10-13 16:02:10 -0400

    Initial version of file

---
+++ pages/about/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+        <Logo></Logo>
+        <h1> {{ title }} </h1>
+    </div>
+</template>
+<script>
+import Logo from '../../components/Logo'
+export default {
+    asyncData() {
+        return {
+            title: 'About Pragra'
+        }
+    }
+}
+</script>
